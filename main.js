d0aa60ce9baca3262d2bf71807cea8e6eb29b653
nukeop <alsw@protonmail.com>
2017-07-05 12:21:12 +0200

    Add safeguards to moveTo to notify on invalid targets

--- main.js
+++ main.js
@@ -14,3 +14,15 @@ module.exports.loop = function() {
 
   }
 }
+
+if (!Creep.prototype._moveTo) {
+  Creep.prototype._moveTo = Creep.prototype.moveTo;
+
+  Creep.prototype.moveTo = function(...args) {
+
+    if(!args[0]) {
+      this.say('ERROR: Invalid moveTo target.')
+    }
+    return this._moveTo.apply(this, args);
+  }
+}
