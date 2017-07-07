91413acdc3af0d9cb0b1f8c7c3a9accd01c0dcf6
nukeop <alsw@protonmail.com>
2017-07-07 15:56:37 +0200

    Simple sentinel behavior

--- role.sentinel.js
+++ role.sentinel.js
@@ -2,11 +2,29 @@ const actions = require('creeps.actions');
 const bodies = require('creeps.bodies');
 
 
+function getOppositeDirection(direction) {
+  return ((direction + 3) % 8) + 1;
+}
+
 /*
   Sentinels guard their rooms
 */
 module.exports = {
   run: function(creep) {
+    var target = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
+    var attacking = false;
+    if (target && creep.attack(target) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(target);
+      attacking = true;
+    }
+
+    // Get away from the spawn if we're not chasing an invader
+    var spawn = creep.pos.findClosestByPath(FIND_MY_SPAWNS);
+    if (!attacking) {
+      if (creep.pos.getRangeTo(spawn) < 3) {
+        creep.move(getOppositeDirection(creep.pos.getDirectionTo(spawn)));
+      }
+    }
 
   },
 
