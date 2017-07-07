8989c3f4d9a4f95fb8077e5e2799031499d607cc
nukeop <alsw@protonmail.com>
2017-07-07 12:07:48 +0200

    Scout attacking priorities

--- creeps.bodies.js
+++ creeps.bodies.js
@@ -2,5 +2,5 @@ module.exports = {
   basic: [WORK, WORK, CARRY, MOVE],
   worker: [WORK, WORK, WORK, WORK, MOVE],
   fast: [MOVE, MOVE, MOVE, MOVE, CARRY],
-  scout: [MOVE, MOVE, MOVE, MOVE, MOVE]
+  scout: [MOVE, MOVE, MOVE, ATTACK, ATTACK]
 };
--- role.scout.js
+++ role.scout.js
@@ -4,9 +4,40 @@ const bodies = require('creeps.bodies');
 module.exports = {
   run: function(creep) {
 
+
+    if (creep.room.controller.my) {
+      creep.moveTo(49, 20);
+    } else {
+      var target = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS);
+      if (creep.attack(target) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(target);
+      }
+
+      if (!target) {
+        target = creep.pos.findClosestByPath(FIND_HOSTILE_SPAWNS);
+        if (creep.attack(target) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(target);
+        }
+
+        if (!target) {
+          target = creep.room.controller;
+          if (creep.attack(target) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(target);
+          }
+
+          if (!target) {
+            creep.moveTo(0, 21);
+          }
+        }
+      }
+
+    }
+
   },
 
   create: function(spawn) {
-    spawn.createCreep(bodies.scout, memory={role: 'scout'});
+    spawn.createCreep(bodies.scout, memory = {
+      role: 'scout'
+    });
   }
 }
