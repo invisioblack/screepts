7de268e9b3223db35f8094d0498565f883de10ce
nukeop <alsw@protonmail.com>
2017-07-08 17:55:30 +0200

    Fix bug where miners would sometimes claim all sources in the room

--- role.miner.js
+++ role.miner.js
@@ -22,6 +22,7 @@ module.exports = {
         if (creep.room.memory.sourcesToMiners[source] == null) {
           creep.memory.mySource = _.find(sources, {'id': source});
           creep.room.memory.sourcesToMiners[source] = creep.id;
+          return;
         }
       }
     } else {
