3f55e4424d0fd844e814e76a053a09079b503ed4
nukeop <alsw@protonmail.com>
2017-07-13 14:01:34 +0200

    Simpler body cost calculation

--- utils.js
+++ utils.js
@@ -1,23 +1,5 @@
 function calculateBodyCost(body) {
-  return _.sum(_.map(body, part => {
-    switch (part) {
-      case MOVE:
-      case CARRY:
-        return 50;
-      case WORK:
-        return 100;
-      case ATTACK:
-        return 80;
-      case RANGED_ATTACK:
-        return 150;
-      case HEAL:
-        return 250;
-      case CLAIM:
-        return 600;
-      case TOUGH:
-        return 10;
-    }
-  }));
+  return _.reduce(body, (sum, part) => sum + BODYPART_COST[part], 0);
 }
 
 function freeSpaces(roomPosition) {
