0496c5a79cc715ececc82c9aaf995d9df00029b9
nukeop <alsw@protonmail.com>
2017-07-13 10:42:05 +0200

    Body building functions 💪

--- creeps.bodies.js
+++ creeps.bodies.js
@@ -1,48 +1,90 @@
 const utils = require('utils');
 
-module.exports = {
-  basic: [WORK, WORK, CARRY, MOVE],
-  worker: [WORK, MOVE],
-  fast: [MOVE, CARRY],
-  scout: [MOVE, MOVE, MOVE, ATTACK, ATTACK],
-  sentinel: [TOUGH, ATTACK, ATTACK, ATTACK, MOVE],
-  reclaimer: [CLAIM, CLAIM, CLAIM, CLAIM, CLAIM, MOVE],
-  remoteminer: [WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE],
-  reserver: [CLAIM, MOVE, MOVE, MOVE, MOVE],
+const addMax = (spawn, body, bodyPart) => {
+  var bodyCost = utils.calculateBodyCost(body);
+  var bodyPartCost = utils.calculateBodyCost([bodyPart]);
 
-  createBasic: () => {
-    return [WORK, CARRY, MOVE];
-  },
-
-  createLargestWorker: (spawn, base=null) => {
-    var body = [WORK, MOVE];
-    if (base != null) {
-      body = base;
+  while (bodyCost < spawn.room.energyAvailable) {
+    if (bodyCost + bodyPartCost > spawn.room.energyAvailable) {
+      break;
     }
 
-    while(utils.calculateBodyCost(body) < spawn.room.energyAvailable) {
-      if (utils.calculateBodyCost(body) + utils.calculateBodyCost([WORK]) > spawn.room.energyAvailable) {
-        break;
-      }
-      body.unshift(WORK);
-    }
-
-    console.log(utils.calculateBodyCost(body), '/', spawn.room.energyAvailable);
-
-    return body;
-  },
-  createFastest: (spawn, base=null) => {
-    var body = [MOVE];
-    if (base != null) {
-      body = base;
-    }
-
-    while(utils.calculateBodyCost(body) < spawn.room.energyAvailable) {
-      if (utils.calculateBodyCost(body) + utils.calculateBodyCost([MOVE]) > spawn.room.energyAvailable) {
-        break;
-      }
-      body.unshift(MOVE);
-    }
-    return body;
+    bodyCost += bodyPartCost;
+    body.unshift(bodyPart);
   }
+};
+
+const createBasic = () => {
+  return [WORK, CARRY, MOVE];
+};
+
+const createLargestWorker = (spawn, base = null) => {
+  var body = [WORK, MOVE];
+  if (base != null) {
+    body = base;
+  }
+  addMax(spawn, body, WORK);
+  return body;
+};
+
+const createFastest = (spawn, base = null) => {
+  var body = [MOVE];
+  if (base != null) {
+    body = base;
+  }
+  addMax(spawn, body, MOVE);
+  return body;
+};
+
+const createStrongestMelee = (spawn, base = null) => {
+  var body = [MOVE];
+  if (base != null) {
+    body = base;
+  }
+  addMax(spawn, body, ATTACK);
+  return body;
+};
+
+module.exports = {
+  basic: [
+    WORK, WORK, CARRY, MOVE
+  ],
+  worker: [
+    WORK, MOVE
+  ],
+  fast: [
+    MOVE, CARRY
+  ],
+  scout: [
+    MOVE, MOVE, MOVE, ATTACK, ATTACK
+  ],
+  sentinel: [
+    TOUGH, ATTACK, ATTACK, ATTACK, MOVE
+  ],
+  reclaimer: [
+    CLAIM,
+    CLAIM,
+    CLAIM,
+    CLAIM,
+    CLAIM,
+    MOVE
+  ],
+  remoteminer: [
+    WORK,
+    WORK,
+    CARRY,
+    CARRY,
+    CARRY,
+    MOVE,
+    MOVE
+  ],
+  reserver: [
+    CLAIM, MOVE, MOVE, MOVE, MOVE
+  ],
+
+  addMax,
+  createBasic,
+  createLargestWorker,
+  createFastest,
+  createStrongestMelee
 };
