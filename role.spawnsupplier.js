93699bef2693c9158f04fe302ef28017942eddba
nukeop <alsw@protonmail.com>
2017-07-14 13:34:24 +0200

    Extra bracket and indentation

--- role.spawnsupplier.js
+++ role.spawnsupplier.js
@@ -5,29 +5,25 @@ module.exports = {
   run: function(creep) {
 
     if (creep.room.energyAvailable < creep.room.energyCapacityAvailable) {
-      if(creep.carry.energy < creep.carryCapacity) {
+      if (creep.carry.energy < creep.carryCapacity) {
         if (!actions.withdrawFromNearestContainer(creep)) {
           if (!actions.withdrawFromNearestStorage(creep)) {
             actions.recycleSelf(creep);
           }
-          }
         }
-      } else {
-        if (!actions.dumpEnergyAt(creep, STRUCTURE_SPAWN)) {
-          if (!actions.dumpEnergyAt(creep, STRUCTURE_EXTENSION)) {
-            actions.recycleSelf(creep);
-          }
+      }
+    } else {
+      if (!actions.dumpEnergyAt(creep, STRUCTURE_SPAWN)) {
+        if (!actions.dumpEnergyAt(creep, STRUCTURE_EXTENSION)) {
+          actions.recycleSelf(creep);
         }
       }
     }
-
-
   },
 
   create: function(spawn) {
-    return spawn.createCreep(
-      bodies.createFastest(spawn, base=[CARRY, CARRY, MOVE, MOVE]),
-      memory={role:'spawnsupplier'}
-    );
+    return spawn.createCreep(bodies.createFastest(spawn, base = [CARRY, CARRY, MOVE, MOVE]), memory = {
+      role: 'spawnsupplier'
+    });
   }
 }
