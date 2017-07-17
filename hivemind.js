c45241553b076abfe4725c71d53d80515a99cda4
nukeop <alsw@protonmail.com>
2017-07-17 20:43:05 +0200

    Ensure structures do not overlap

--- hivemind.js
+++ hivemind.js
@@ -39,12 +39,18 @@ hivemind.planRoom = () => {
         roomInst.memory.plan = {};
       }
 
+      if (!roomInst.memory.plan.taken) {
+        roomInst.memory.plan.taken = [];
+      }
+
       if (!roomInst.memory.plan.storage) {
         roomInst.memory.plan.storage = roomInst.controller.pos.findNearPosition().next().value;
+        roomInst.memory.plan.taken.push(roomInst.memory.plan.storage);
       }
 
       if (!roomInst.memory.plan.upgrader) {
         roomInst.memory.plan.upgrader = new RoomPosition(roomInst.memory.plan.storage.x, roomInst.memory.plan.storage.y, roomInst.memory.plan.storage.roomName).findNearPosition().next().value;
+        roomInst.memory.plan.taken.push(roomInst.memory.plan.upgrader);
       }
 
       if (!roomInst.memory.plan.roads) {
@@ -67,11 +73,13 @@ hivemind.planRoom = () => {
           for (var source in sources) {
             road = hivemind.roadFromTo(spawns[spawn].pos, sources[source].pos);
             roads.push(road.path);
+            roomInst.memory.plan.taken.push(...road.path);
           }
 
           // Connect spawns to room controller
           road = hivemind.roadFromTo(spawns[spawn].pos, roomInst.controller.pos);
           roads.push(road.path);
+          roomInst.memory.plan.taken.push(...road.path);
 
           // Connect spawns to exits
           var exits = [spawns[spawn].pos.findClosestByPath(FIND_EXIT_TOP), spawns[spawn].pos.findClosestByPath(FIND_EXIT_BOTTOM), spawns[spawn].pos.findClosestByPath(FIND_EXIT_RIGHT), spawns[spawn].pos.findClosestByPath(FIND_EXIT_LEFT)];
@@ -80,6 +88,7 @@ hivemind.planRoom = () => {
             if (exits[exit]) {
               road = hivemind.roadFromTo(spawns[spawn].pos, exits[exit]);
               roads.push(road.path);
+              roomInst.memory.plan.taken.push(...road.path);
             }
           }
         }
@@ -91,7 +100,19 @@ hivemind.planRoom = () => {
       if (!roomInst.memory.plan.extensions) {
         roomInst.memory.plan.extensions = [];
         var longestRoad = _.last(_.sortBy(roomInst.memory.plan.roads, road => road.length));
-        var freePositions = _.flatten(_.uniq(_.map(longestRoad, rp => {return Array.from(rp.findNearPosition())})));
+        var freePositions = _.flatten(_.map(longestRoad, rp => {
+          return Array.from(rp.findNearPosition())
+        }));
+
+        // Remove duplicate positions and taken positions
+        var cleaned = [];
+        freePositions = _.forEach(freePositions, position => {
+          if (!_.some(cleaned, dupe => dupe.x == position.x && dupe.y == position.y) && !_.some(roomInst.memory.plan.taken, dupe => dupe.x == position.x && dupe.y == position.y)) {
+            cleaned.push(position);
+            roomInst.memory.plan.taken.push(position);
+          }
+        });
+        freePositions = cleaned;
 
         for (var i = 0; i < utils.getExtensionsAtRCL(roomInst.controller.level); i++) {
           roomInst.memory.plan.extensions.push(freePositions[i]);
