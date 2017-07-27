8e5c0cb852170f08076b325b2dcbc9a8fed9a705
nukeop <alsw@protonmail.com>
2017-07-28 01:24:29 +0200

    optimize room state caching

--- rooms.main.js
+++ rooms.main.js
@@ -5,31 +5,36 @@ module.exports = {
       return;
     }
 
-    room.memory.initialized = true;
-    room.memory.my = room.controller && room.controller.my;
+    let memory = {};
+    memory.initialized = true;
+    memory.my = room.controller && room.controller.my;
 
-    if(!room.memory.my){
+    if(!memory.my){
+      room.memory = memory;
       return;
     }
 
-    room.memory.sources = room.find(FIND_SOURCES);
-    room.memory.minerals = room.find(FIND_MINERALS);
-    room.memory.exits = [
+    memory.sources = room.find(FIND_SOURCES);
+    memory.minerals = room.find(FIND_MINERALS);
+    memory.exits = [
       room.find(FIND_EXIT_TOP),
       room.find(FIND_EXIT_BOTTOM),
       room.find(FIND_EXIT_RIGHT),
       room.find(FIND_EXIT_LEFT),
     ];
 
+    room.memory = memory;
   },
 
   roomBehavior: (room) => {
     if (room.memory.my) {
-      room.memory.droppedEnergy = room.find(FIND_DROPPED_RESOURCES, {filter: {resourceType: RESOURCE_ENERGY}});
-      room.memory.structuresByType = _.groupBy(room.find(FIND_STRUCTURES), 'structureType');
-      room.memory.constructionSites = room.find(FIND_CONSTRUCTION_SITES);
-      room.memory.myCreeps = room.find(FIND_MY_CREEPS);
-      room.memory.myCreepsByRole = _.groupBy(room.memory.myCreeps, creep => creep.memory.role);
+      let memory = _.clone(room.memory);
+      memory.droppedEnergy = room.find(FIND_DROPPED_RESOURCES, {filter: {resourceType: RESOURCE_ENERGY}});
+      memory.structuresByType = _.groupBy(room.find(FIND_STRUCTURES), 'structureType');
+      memory.constructionSites = room.find(FIND_CONSTRUCTION_SITES);
+      memory.myCreeps = room.find(FIND_MY_CREEPS);
+      memory.myCreepsByRole = _.groupBy(memory.myCreeps, creep => creep.memory.role);
+      room.memory = memory;
     }
 
 
