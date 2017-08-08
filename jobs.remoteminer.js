1a627d07da1aa98cd60fae09e1615b655e3b5c05
nukeop <alsw@protonmail.com>
2017-08-08 11:44:50 +0200

    make remote miners come back when full

--- jobs.remoteminer.js
+++ jobs.remoteminer.js
@@ -37,12 +37,36 @@ module.exports = {
         }
 
       } else {
-        let target = miner.pos.findClosestByPath(FIND_SOURCES);
-        if (target) {
-          miner.memory.job = {
-            action: 'mine',
-            room: miner.pos.roomName,
-            target: target.id
+        if (miner.carry.energy < miner.carryCapacity) {
+          let target = miner.pos.findClosestByPath(FIND_SOURCES);
+          if (target) {
+            miner.memory.job = {
+              action: 'mine',
+              room: miner.pos.roomName,
+              target: target.id
+            }
+          }
+        } else {
+          let target = originRoom.storage;
+          if (target) {
+            miner.memory.job = {
+              action: 'dumpEnergy',
+              room: miner.memory.originRoom,
+              target: target.id
+            };
+
+          } else {
+            let target = originRoom.controller.pos.findClosestByPath(originRoom.memory.structuresByType.extension, {
+              filter: ext => ext.energy < ext.energyCapacity
+            });
+
+            if (target) {
+              miner.memory.job = {
+                action: 'dumpEnergy',
+                room: miner.memory.originRoom,
+                target: target.id
+              };
+            }
           }
         }
       }
