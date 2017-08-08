4e36be62bcd0596a1a27b9184b63eda265586446
nukeop <alsw@protonmail.com>
2017-08-08 11:40:40 +0200

    optimize remote miner

--- jobs.remoteminer.js
+++ jobs.remoteminer.js
@@ -1,48 +1,48 @@
-module.exports ={
+module.exports = {
   assignJobs: (room, creeps) => {
     _.forEach(creeps, miner => {
       let originRoom = Game.rooms[miner.memory.originRoom];
       let targetRoom = Game.rooms[miner.memory.targetRoom];
 
-      if (miner.carry.energy < miner.carryCapacity) {
-          if(miner.pos.roomName != miner.memory.targetRoom) {
-            miner.memory.job = {
-              action: 'remoteMine',
-              room: miner.memory.targetRoom,
-              target: null
-            };
-          } else {
-            let target = miner.pos.findClosestByPath(FIND_SOURCES);
-            if (target) {
-              miner.memory.job = {
-                action: 'mine',
-                room: miner.pos.roomName,
-                target: target.id
-              }
-            }
-          }
-
-      } else {
-        let target = originRoom.storage;
-        if (target) {
-          miner.memory.job = {
-            action: 'dumpEnergy',
-            room: miner.memory.originRoom,
-            target: target.id
-          };
-
-        } else {
-          let target = originRoom.controller.pos.findClosestByPath(
-            originRoom.memory.structuresByType.extension,
-            {filter: ext => ext.energy < ext.energyCapacity}
-          );
-
-          if(target) {
+      if (miner.pos.roomName != miner.memory.targetRoom) {
+        if (miner.carry.energy > 0) {
+          let target = originRoom.storage;
+          if (target) {
             miner.memory.job = {
               action: 'dumpEnergy',
               room: miner.memory.originRoom,
               target: target.id
             };
+
+          } else {
+            let target = originRoom.controller.pos.findClosestByPath(originRoom.memory.structuresByType.extension, {
+              filter: ext => ext.energy < ext.energyCapacity
+            });
+
+            if (target) {
+              miner.memory.job = {
+                action: 'dumpEnergy',
+                room: miner.memory.originRoom,
+                target: target.id
+              };
+            }
+          }
+        } else {
+          miner.memory.job = {
+            action: 'remoteMine',
+            room: miner.memory.targetRoom,
+            target: null
+          };
+
+        }
+
+      } else {
+        let target = miner.pos.findClosestByPath(FIND_SOURCES);
+        if (target) {
+          miner.memory.job = {
+            action: 'mine',
+            room: miner.pos.roomName,
+            target: target.id
           }
         }
       }
