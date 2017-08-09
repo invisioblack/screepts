bdfffc65a1451ef33f2e4c055cf3f171530e377f
nukeop <alsw@protonmail.com>
2017-08-09 21:28:56 +0200

    links in courier jobs

--- jobs.courier.js
+++ jobs.courier.js
@@ -41,6 +41,7 @@ module.exports = {
               let closestLink = courier.pos.findClosestByPath(links, {
                 filter: link => link.energy < link.energyCapacity && link.id != storageLink.id
               });
+              console.log(closestLink);
               target = courier.room.storage;
               if (courier.pos.findPathTo(closestLink).length < courier.pos.findPathTo(target)) {
                 target = closestLink;
@@ -57,48 +58,58 @@ module.exports = {
           }
         }
       } else {
-        let droppedEnergy = _(room.memory.droppedEnergy).map(de => Game.getObjectById(de.id)).filter(de => de.amount > 50).sortBy(de => de.amount).reverse().value();
-        let target = _.head(droppedEnergy);
-        if (target) {
+        let target;
+        if (storageLink.energy / storageLink.energyCapacity > 0.75) {
+          target = storageLink;
           courier.memory.job = {
-            action: 'collectResource',
-            room: courier.pos.roomName,
+            action: 'withdrawEnergy',
+            room: room.name,
             target: target.id
-          };
-
-          target.amount -= 300;
-          if (target.amount <= 0) {
-            _.remove(droppedEnergy, dropped => dropped.id == target.id);
           }
-        } else if (room.terminal && room.terminal.store[RESOURCE_ENERGY] > room.terminal.storeCapacity * 0.075) {
-          target = room.terminal;
+        } else {
+          let droppedEnergy = _(room.memory.droppedEnergy).map(de => Game.getObjectById(de.id)).filter(de => de.amount > 50).sortBy(de => de.amount).reverse().value();
+          target = _.head(droppedEnergy);
           if (target) {
             courier.memory.job = {
-              action: 'withdrawEnergy',
+              action: 'collectResource',
               room: courier.pos.roomName,
               target: target.id
-            }
-          }
-        } else if (room.memory.structuresByType.container && room.memory.structuresByType.container.length > 0) {
-          var containers = _.map(_.filter(room.memory.structuresByType.container, container => container.store[RESOURCE_ENERGY] > 0), container => Game.getObjectById(container.id));
-          target = courier.pos.findClosestByPath(containers);
-          if(target) {
-            courier.memory.job = {
-              action: 'withdrawEnergy',
-              room: courier.pos.roomName,
-              target: target.id
-            }
+            };
 
-            target.store[RESOURCE_ENERGY] -= 300;
+            target.amount -= 300;
             if (target.amount <= 0) {
-              _.remove(containers, container => container.id == target.id);
+              _.remove(droppedEnergy, dropped => dropped.id == target.id);
             }
-          } else {
-            target = courier.pos.findClosestByPath(room.memory.structuresByType.spawn);
+          } else if (room.terminal && room.terminal.store[RESOURCE_ENERGY] > room.terminal.storeCapacity * 0.075) {
+            target = room.terminal;
             if (target) {
               courier.memory.job = {
-                action: 'recycleSelf',
+                action: 'withdrawEnergy',
+                room: courier.pos.roomName,
                 target: target.id
+              }
+            }
+          } else if (room.memory.structuresByType.container && room.memory.structuresByType.container.length > 0) {
+            var containers = _.map(_.filter(room.memory.structuresByType.container, container => container.store[RESOURCE_ENERGY] > 0), container => Game.getObjectById(container.id));
+            target = courier.pos.findClosestByPath(containers);
+            if (target) {
+              courier.memory.job = {
+                action: 'withdrawEnergy',
+                room: courier.pos.roomName,
+                target: target.id
+              }
+
+              target.store[RESOURCE_ENERGY] -= 300;
+              if (target.amount <= 0) {
+                _.remove(containers, container => container.id == target.id);
+              }
+            } else {
+              target = courier.pos.findClosestByPath(room.memory.structuresByType.spawn);
+              if (target) {
+                courier.memory.job = {
+                  action: 'recycleSelf',
+                  target: target.id
+                }
               }
             }
           }
