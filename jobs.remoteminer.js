f8c42c1d4cb999696241af52605de4b41e5bafb3
nukeop <alsw@protonmail.com>
2017-08-10 08:44:38 +0200

    update

--- jobs.remoteminer.js
+++ jobs.remoteminer.js
@@ -1,8 +1,12 @@
 module.exports = {
   assignJobs: (room, creeps) => {
+
     _.forEach(creeps, miner => {
       let originRoom = Game.rooms[miner.memory.originRoom];
       let targetRoom = Game.rooms[miner.memory.targetRoom];
+
+      let links = _.map(originRoom.memory.structuresByType.link, link => Game.getObjectById(link.id));
+      let storageLink = Game.getObjectById(originRoom.memory.storageLink.id);
 
       if (miner.pos.roomName != miner.memory.targetRoom) {
         if (miner.carry.energy > 0) {
@@ -49,7 +53,17 @@ module.exports = {
             }
           }
         } else {
+          let closestLink = miner.pos.findClosestByPath(links, {
+            filter: link => link.energy < link.energyCapacity && (storageLink ? link.id != storageLink.id : true)
+          });
+
           let target = originRoom.storage;
+
+          if (closestLink && miner.pos.findPathTo(closestLink).length < miner.pos.findPathTo(target).length) {
+            target = closestLink;
+          }
+
+
           if (target) {
             miner.memory.job = {
               action: 'dumpEnergy',
